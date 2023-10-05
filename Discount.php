b7ad8fac46cbce1154d19b1367752a5ed3d7dffb
ilushkai <ilya.ilya.ignatenko@gmaol.com>
2023-10-05 13:11:07 +1000

    добавил класс Discount со скидками

---
+++ Discount.php
@@ -0,0 +1,9 @@
+<?php
+
+class Discounts {
+    public static $discounts = [
+        'holiday' => ['discount_percent' => 5, 'city' => 'vl'],
+        'hotprice' => ['discount_percent' => 3, 'city' => 'all'],
+        'promo' => ['discount_percent' => 6, 'city' => 'khv']
+    ];
+}
